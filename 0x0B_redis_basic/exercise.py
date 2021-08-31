713fc0e30fc6787d3d2c6223efe5fdb9a2cb9c61
Manuelpv17 <manuelpv17@outlook.com>
2021-08-31 16:54:23 -0500

    add recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -15,8 +15,30 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """store Method"""
+        """store string in redis"""
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
 
         return random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ recover value """
+        if fn:
+            return fn(self._redis.get(key))
+
+        return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        """ to str """
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """ to int """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
