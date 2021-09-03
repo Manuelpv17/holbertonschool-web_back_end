095deed094e0d16bc450a79965020fe46e9207e5
Manuelpv17 <manuelpv17@outlook.com>
2021-09-02 19:30:29 -0500

    add create users sql script

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,5 @@
 -- creates a table users following these requirements:
--- With these attributes:
--- id, integer, never null, auto increment and primary key
--- email, string (255 characters), never null and unique
--- name, string (255 characters)
+-- id, integer, never null, auto increment and primary key / email, string (255 characters), never null and unique / name, string (255 characters)
 CREATE TABLE IF NOT EXIST users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
