00fdba23361b0d7c4e4d730ab362faf1e0659c57
Manuelpv17 <manuelpv17@outlook.com>
2021-09-02 19:58:02 -0500

    fix typo

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,10 @@
 -- creates a table users following these requirements:
--- id, integer, never null, auto increment and primary key / email, string (255 characters), never null and unique / name, string (255 characters)
-CREATE TABLE IF NOT EXIST users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+-- With these attributes:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
