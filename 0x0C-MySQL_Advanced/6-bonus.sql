b6bd42dc99e5b59ab536a0734c327d51af319d5f
Manuelpv17 <manuelpv17@outlook.com>
2021-09-02 22:28:28 -0500

    Add task 5 and 6

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -10,7 +10,7 @@ CREATE PROCEDURE AddBonus(
 BEGIN
     INSERT INTO projects(name) SELECT project_name FROM DUAL
     WHERE NOT EXISTS (SELECT * FROM projects WHERE name = project_name LIMIT 1);
-    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
+ INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, (SELECT id FROM projects WHERE name = project_name), score);
 END
 $$
 DELIMITER ;
