27c6741a4a02a7a98c0a8c07c442c153b13128ea
Manuelpv17 <manuelpv17@outlook.com>
2021-09-07 14:26:56 -0500

    Add task 12

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python3
-""" some stats of nginx logs
-"""
+""" 12. Log stats - pymongo """
+
 from pymongo import MongoClient
-
-
-# def log_stats(collection):
-#     return collection.find()
 
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
+    logs = collection.count_documents({})
+    print(f'{logs} logs')
+
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    documents = collection.count_documents({})
-    print(f"{documents} logs")
-    print("Methods:")
+    print('Methods:')
     for method in methods:
         count = collection.count_documents({"method": method})
         print(f'\tmethod {method}: {count}')
 
-    status_check = collection.count_documents(
+    status = collection.count_documents(
         {"method": "GET", "path": "/status"}
-        )
-    print(f'{status_check} status check')
+    )
+
+    print(f'{status} status check')
