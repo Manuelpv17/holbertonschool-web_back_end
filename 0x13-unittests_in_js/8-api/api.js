4122c8e931e813d61bc8096282dff4c6519762fc
Manuelpv17 <manuelpv17@outlook.com>
2021-09-29 22:55:25 -0500

    Add task 8

--- 0x13-unittests_in_js/8-api/api.js
+++ 0x13-unittests_in_js/8-api/api.js
@@ -1,13 +1,14 @@
 const express = require("express");
 
 const app = express();
+const port = 7865;
 
 app.get("/", (req, res) => {
-  res.end("Welcome to the payment system");
+  res.send("Welcome to the payment system");
 });
 
 app.listen(port, () => {
-  console.log("API available on localhost port 7865");
+  console.log(`API available on localhost port ${port}`);
 });
 
 module.exports = app;
