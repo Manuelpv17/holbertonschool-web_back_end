8fa7a2b66022492057bd521ea3e427fc6e037006
Manuelpv17 <manuelpv17@outlook.com>
2021-09-29 21:49:51 -0500

    Add task 3, spies

---
+++ 0x13-unittests_in_js/utils.js
@@ -0,0 +1,23 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    const numA = Math.round(a);
+    const numB = Math.round(b);
+
+    if (Number.isNaN(numA) || Number.isNaN(numB)) {
+      throw TypeError;
+    }
+
+    if (type === "SUM") {
+      return numA + numB;
+    } else if (type === "SUBTRACT") {
+      return numA - numB;
+    } else if (type === "DIVIDE") {
+      if (numB === 0) return "Error";
+      return numA / numB;
+    } else {
+      throw TypeError;
+    }
+  },
+};
+
+module.exports = Utils;
