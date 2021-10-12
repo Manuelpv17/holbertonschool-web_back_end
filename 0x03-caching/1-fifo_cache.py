42e973b5ca735ba77223aed8455be5cec6eff67b
Manuelpv17 <manuelpv17@outlook.com>
2021-10-12 09:32:40 -0500

    add task 1, fifi cache

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" 1. FIFO caching """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ 1. FIFO caching """
+
+    def __init__(self):
+        """ Constructor """
+        super().__init__()
+        self.FIFO = []
+
+    def put(self, key, item):
+        """ Put in cache """
+
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                return
+
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded = self.FIFO.pop(0)
+                del self.cache_data[discarded]
+                print("DISCARD:", discarded)
+
+            self.cache_data[key] = item
+            self.FIFO.append(key)
+
+    def get(self, key):
+        """ get from cache """
+
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
